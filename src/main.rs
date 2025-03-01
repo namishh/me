a8cd9c82c1ceafcd3a25a710b995f59ddc4106c1
namish <namish@tuta.io>
2025-03-01 18:33:56 +0530

    single html page

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{middleware, web, App, Error, HttpServer, Responder, Result};
+use std::collections::HashMap;
+use tera::Tera;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, World!")
+async fn index(
+    tmpl: web::Data<tera::Tera>,
+    _: web::Query<HashMap<String, String>>,
+) -> Result<impl Responder, Error> {
+    let html = tmpl
+        .render("index.html", &tera::Context::new())
+        .expect("Failed to render template");
+    Ok(web::Html::new(html))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    HttpServer::new(move || {
+        let tera = match Tera::new("templates/**/*.html") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
         App::new()
-            .service(hello) // Register the route
+            .app_data(web::Data::new(tera))
+            .wrap(middleware::Logger::default())
+            .service(actix_files::Files::new("/static", "./static").show_files_listing())
+            .service(web::resource("/").route(web::get().to(index)))
     })
-    .bind("0.0.0.0:8080")? // Bind to localhost on port 8080
+    .bind("0.0.0.0:8080")? 
     .run()
     .await
 }
